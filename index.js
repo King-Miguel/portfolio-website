1a05e950aa0236e70ce37d0b8a48d0dedc01efbf
King-Miguel <iggytesoro123@gmail.com>
2026-07-20 18:10:41 +0800

    Fix static file serving

--- index.js
+++ index.js
@@ -2,27 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files
+// Serve ALL static folders
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'models')));
-app.use(express.static(path.join(__dirname, 'views')));
+app.use('/images', express.static(path.join(__dirname, 'public', 'images')));
+app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
+app.use('/js', express.static(path.join(__dirname, 'public', 'js')));
+app.use('/models', express.static(path.join(__dirname, 'models')));
 
-// Routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.get('/calculator', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'calcu.html'));
-});
-
-// Export for Vercel
 module.exports = app;
 
-// Local development
 if (require.main === module) {
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
-        console.log(`🏰 Portfolio running on http://localhost:${PORT}`);
+        console.log(`🏰 Server running on ${PORT}`);
     });
 }
